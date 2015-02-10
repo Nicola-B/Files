7e40b368d73d8dcb5f06256ecc84b4fca353f9fd
40340 <40340@PC004139.coll.lr.local>
2015-02-10 13:00:14 +0000

    Commit

---
+++ test_data_exercise.py
@@ -0,0 +1,30 @@
+#Nicola Batty
+#10/02/2015
+#files class exercises task 1
+
+with open("test_data.txt", mode="r", encoding="utf-8") as test_data:
+    def get_test_results():
+        test_results = []
+        for line in test_data:
+            for test in range(3):
+                test_results.append(int(line))
+        return test_results
+
+    def average_test_result(results):
+        total = 0
+        for test in results:
+            total += test
+        average = total/len(results)
+        return average
+
+    def display_average_result(average):
+        print("your average test result is: {0}".format(average))
+
+
+    def main():
+        test_results = get_test_results()
+        average = average_test_result(test_results)
+        display_average_result(average)
+
+    if __name__ == "__main__":
+        main()
