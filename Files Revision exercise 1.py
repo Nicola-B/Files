2555e9d0112819c0b9237898a39d9fc0be66c94b
40340 <40340@PC004139.coll.lr.local>
2015-02-13 14:32:18 +0000

    commit

---
+++ Files Revision exercise 1.py
@@ -0,0 +1,14 @@
+#Nicola Batty
+#13/02/2015
+#Files Revision exercise 1
+
+with open("lines.txt", mode="w", encoding="utf-8") as lines:
+    lines.write(input("Line 1: "))
+    lines.write("/n")
+    lines.write(input("Line 2: "))
+    lines.write("/n")
+    lines.write(input("Line 3: "))
+    lines.write("/n")
+    lines.write(input("Line 4: "))
+    lines.write("/n")
+    lines.write(input("Line 5: "))
